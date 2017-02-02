648391c9c3c900d6f9775253206b5882c0a54b52
waski35 <arma@48h.waw.pl>
2017-02-02 20:19:24 +0100

    deleted classic flash service

--- SMWebManager/app/config/services_web.php
+++ SMWebManager/app/config/services_web.php
@@ -47,14 +47,7 @@ $di->setShared('session', function () {
 /**
  * Register the session flash service with the Twitter Bootstrap classes
  */
-$di->set('flash', function () {
-    return new Flash(array(
-        'error'   => 'alert alert-danger',
-        'success' => 'alert alert-success',
-        'notice'  => 'alert alert-info',
-        'warning' => 'alert alert-warning'
-    ));
-});
+
 
 $di->set(
     "flashsession",
